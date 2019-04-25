f37a7e5563b501ecef517fa722d14b2fb60c4242
Zein Miftah <zeinmiftah@gmail.com>
2019-04-25 17:15:46 +0700

    change lamus to user

--- apache-php7.1/docker/init.sql
+++ apache-php7.1/docker/init.sql
@@ -4,5 +4,5 @@ create database pkpu_core;
 use pkpu_core;
 source /db/database.sql;
 
-create user lamus identified by "password";
-grant all privileges on pkpu_core.* to lamus@'%';
+create user user identified by "password";
+grant all privileges on pkpu_core.* to user@'%';
